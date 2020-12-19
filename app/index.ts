c0c785330ec551c22d5faa019ab974fcd545adc8
Barb <clockery69@gmail.com>
2020-12-19 16:12:47 +0100

    variations of inline animations

--- app/index.ts
+++ app/index.ts
@@ -153,31 +153,48 @@ for (i = 0; i < numChars ; i++) {
 
  };
 //ANIMATION----------------------------------------------------------------------------------------------
+let textChars = document.getElementById("textChars") as GroupElement;
+
 // Rotate on time
+
+const cos = (n) => {
+n = Math.cos(n*Math.PI/180);
+  return n;
+}
+
+
 const initRotation = () => {
 const now = new Date();
+
   let angleSeconds = (now.getSeconds()* 6);
   let angleSmoothSeconds = (now.getSeconds() * 1000 + now.getMilliseconds()) * 6 / 1000;
 
-  curvedTextWidget1.startAngle = angleSeconds; // much better in on tick, or if throttled. but just to keep it together
-  curvedTextWidget2.startAngle = angleSmoothSeconds; // great, you introduced the arc :)
+   // much better in on tick, or if throttled. but just to keep it together
+  curvedTextWidget2.startAngle = 360 + angleSmoothSeconds; // great, you introduced the arc :) 
+  
 
+  textChars.style.opacity = cos(3*angleSmoothSeconds);  // rotation in "auto" modus is understandably rather laggy. best to keep this for "fix"?
+                                                 
   requestAnimationFrame(initRotation);
 }
 requestAnimationFrame(initRotation);
 
 
 clock.granularity = "seconds"
-let textChars = document.getElementById("textChars") as GroupElement;
+
 // Opacity
   clock.ontick = (evt) => {
   const now = new Date();
   let seconds = now.getSeconds();
-  
+  //flash on/off
   //@ts-ignore
   //curvedText2.opacity = (seconds % 2) = 0 ?  1 : 0; //cant reach curvedWidgetText2 from here
+
   //@ts-ignore
-  textChars.style.opacity = (seconds % 2) == 0 ?  1 : 0;
+  //textChars.style.opacity = (seconds % 2) == 0 ?  1 : 0;
   //console.log(textChars.style.opacity);
-  };
+  curvedTextWidget1.startAngle = seconds*6;
+  textChars.style.fill = "#18d6cd" ;
+ 
+};
 // could also change colors, smooth or in ontick; font-size, I guess, position of the circle, radius.....
